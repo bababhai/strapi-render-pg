ef453e0664b4bf7680847ab7f61533beff88ca98
DrCoder <shakil.7878@gmail.com>
2024-11-01 03:03:58 +0530

    6th initial commit 3rd

--- config/database.ts
+++ config/database.ts
@@ -9,9 +9,8 @@
           user: env('DATABASE_USERNAME'),
           password: env('DATABASE_PASSWORD'),
           schema: env('DATABASE_SCHEMA', 'public'), // Not required
-          ssl: {
-            rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-          },
+          ssl: env.bool('DATABASE_SSL_SELF', false) ? { rejectUnauthorized: false } : false,
+
         },
         debug: false,
       },
