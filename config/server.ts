3d9e142e8a56e0d596b73f706fe190c08685bf5b
DrCoder <shakil.7878@gmail.com>
2024-11-01 19:45:22 +0530

    change in server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', process.env.PORT || 10000), // Use the environment PORT or default to 10000
   app: {
     keys: env.array('APP_KEYS'),
   },
