37d92e4a6b286c55aff1179c23edaefdb3eca218
DrCoder <shakil.7878@gmail.com>
2024-11-01 21:38:19 +0530

    change in server.ts 4

--- config/server.ts
+++ config/server.ts
@@ -1,6 +1,6 @@
 export default ({ env }) => ({
-  host: '0.0.0.0', // Ensure the host is set to 0.0.0.0
-  port: parseInt(process.env.PORT) || 10000, // Prioritize Render's PORT, fallback to 10000
+  host: '0.0.0.0', // Ensures the server binds to 0.0.0.0
+  port: parseInt(process.env.PORT), // Use the environment PORT provided by Render
   app: {
     keys: env.array('APP_KEYS'),
   },
